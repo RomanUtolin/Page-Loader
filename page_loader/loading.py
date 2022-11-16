1ec2d788e3aa26ef7a0278cca4e9f83d1502b738
RomanUtolin <romautolin@gmail.com>
2022-11-16 19:28:59 +0300

    add v1 loading_resources.py

--- page_loader/loading.py
+++ page_loader/loading.py
@@ -12,6 +12,6 @@ def download(url, output):
     path_to_html = os.path.join(output, f'{new_url}.html')
     path_to_files = os.path.join(output, f'{new_url}_files')
     html = requests.get(url)
-    loading_resources.download(html, path_to_files, domain)
     html = loading_img.download(html, path_to_files, domain)
+    html = loading_resources.download(html, path_to_files, domain)
     return loading_html.download(html, path_to_html)
--- page_loader/loading_resources.py
+++ page_loader/loading_resources.py
@@ -4,14 +4,37 @@ from bs4 import BeautifulSoup
 
 
 def download(html, path_to_files, domain):
-    html = html.text
+    html = BeautifulSoup(html, 'html.parser')
     if not os.path.isdir(path_to_files):
         os.mkdir(path_to_files)
-    soup = BeautifulSoup(html, 'html.parser')
-    list_link = soup.find_all('link')
-    list_script = soup.find_all('script')
-    for link in list_link:
-        print(link['href'])
+    list_link = html.find_all('link')
+    list_script = html.find_all('script')
+    for href in list_link:
+        link = href['href']
+        if link.startswith('http') or link.startswith('//'):
+            continue
+        else:
+            if not link.startswith('/'):
+                link = f'/{link}'
+                link = f'{domain}{link}'
+                path_to_link = f'{path_to_files}/{link.split("/")[-1]}'
+                link_data = requests.get(link).content
+                with open(path_to_link, 'wb') as f:
+                    f.write(link_data)
+                href['href'] = path_to_link
     for script in list_script:
         if script.has_attr('src'):
-            print('script')
+            link = script['src']
+            if link.startswith('http') or link.startswith('//'):
+                continue
+            else:
+                if not link.startswith('/'):
+                    link = f'/{link}'
+                    link = f'{domain}{link}'
+                    path_to_link = f'{path_to_files}/{link.split("/")[-1]}'
+                    link_data = requests.get(link).content
+                    with open(path_to_link, 'wb') as f:
+                        f.write(link_data)
+                    script['src'] = path_to_link
+
+    return html.prettify()
