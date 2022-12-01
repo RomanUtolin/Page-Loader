825aa3330f7c3df65fee0bf1dbcd05f36524625a
RomanUtolin <romautolin@gmail.com>
2022-12-02 00:25:44 +0300

    test

--- page_loader/resources.py
+++ page_loader/resources.py
@@ -19,23 +19,24 @@ def download(html, path_to_files, hostname, tag):
             link = t[tags_link[tag]]
             if link.startswith('https') or link.startswith('//'):
                 continue
-            else:
-                if not link.startswith('/'):
-                    link = f'/{link}'
-                link = f'{hostname}{link}'
-                path_to_link = f'{path_to_files}/{link.split("/")[-1]}'
-                try:
-                    get_link = requests.get(link)
-                    get_link.raise_for_status()
-                    link_data = get_link.content
-                    save.save_file(link_data, path_to_link)
-                    t[tags_link[tag]] = path_to_link
-                except (requests.exceptions.HTTPError,
-                        ConnectionError,
-                        OSError) as e:
-                    debug_info = (e, e.__class__, e.__traceback__)
-                    logging.debug(debug_info)
-                    logging.warning(
-                        f"{link} wasn't downloaded")
+            elif link.startswith('http'):
+                link = link[16:]
+            elif not link.startswith('/'):
+                link = f'/{link}'
+            link = f'{hostname}{link}'
+            path_to_link = f'{path_to_files}/{link.split("/")[-1]}'
+            try:
+                get_link = requests.get(link)
+                get_link.raise_for_status()
+                link_data = get_link.content
+                save.save_file(link_data, path_to_link)
+                t[tags_link[tag]] = path_to_link
+            except (requests.exceptions.HTTPError,
+                    ConnectionError,
+                    OSError) as e:
+                debug_info = (e, e.__class__, e.__traceback__)
+                logging.debug(debug_info)
+                logging.warning(
+                    f"{link} wasn't downloaded")
 
     return soup.prettify()
